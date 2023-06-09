cf3ff64165ff8d2e638c79a8383de62e3a98bd32
Andrew Hancock <AndrewNeilHancock@gmail.com>
2023-06-09 19:59:23 -0400

    Added basic setup tools integration.

--- src/anh/mil_sim/unit_builder/prompts.py
+++
@@ -1,4 +0,0 @@
-from rich.text import Text
-
-main_menu = Text("L\tList all\nI\tAdd Individual\nW\tAdd Weapon\nS\tSave")
-
--- src/anh/mil_sim/weapon.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-
-class Weapon(BaseModel):
-    name: str
-    range: int
-    damage: int
-    penetration: int
-
